0fbbbefdeedad64ffc5f72d687c041b9f1de20ba
frantastico <fomartinmail@gmail.com>
2025-01-25 10:02:31 -0800

    Gun and Bubble prefabs
    Gun spawns bubble on mouse input.

---
+++ ProjectClean/Assets/Entities/Gun/GunController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.ComponentModel;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    [SerializeField] // Serializing to show in Inspector for debugging. Don't want to make a PropertyDrawer
+    private bool isFireable = true;
+
+    [SerializeField]
+    private float fireCooldownTime = 1f;
+
+    [SerializeField]
+    private Transform bubbleSpawnTransform;
+
+    [SerializeField]
+    private GameObject bubbleGameObject;
+
+    void Update()
+    {
+        if (isFireable && Input.GetMouseButton(0))
+        {
+            SpawnBubble();
+        }
+    }
+
+    void SpawnBubble()
+    {
+        if (bubbleSpawnTransform == null)
+        {
+            bubbleSpawnTransform = this.transform;
+        }
+
+        if (bubbleGameObject == null)
+        {
+            bubbleGameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        }
+        else
+        {
+            GameObject bubbleInstance = GameObject.Instantiate(bubbleGameObject);
+            bubbleInstance.transform.position = bubbleSpawnTransform.position;
+            bubbleInstance.transform.rotation = bubbleSpawnTransform.rotation;
+        }
+
+
+        StartCoroutine(FireCooldownCoroutine(fireCooldownTime));   
+    }
+
+    IEnumerator FireCooldownCoroutine(float fireCooldownTime)
+    {
+        isFireable = false;
+        yield return new WaitForSeconds(fireCooldownTime);
+        isFireable = true;
+    }
+}
