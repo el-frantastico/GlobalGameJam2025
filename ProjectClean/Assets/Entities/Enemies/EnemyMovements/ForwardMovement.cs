d1373a1c710877e40d5c01cb26e65c2f1d98d3d9
frantastico <fomartinmail@gmail.com>
2025-01-25 16:48:30 -0800

    Enemy movement

--- ProjectClean/Assets/Entities/Enemies/EnemyMovements/ForwardMovement.cs
+++ ProjectClean/Assets/Entities/Enemies/EnemyMovements/ForwardMovement.cs
@@ -1,16 +1,46 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ForwardMovement : MonoBehaviour
 {
+
+    [SerializeField]
+    private float speed;
+
+    private new Rigidbody rigidbody;
+
+    HashSet<GameObject> hitObjects;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        hitObjects = new HashSet<GameObject>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        rigidbody.linearVelocity = new Vector3(speed, 0, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 6) //Floor
+        {
+            return;
+        }
+
+        if (hitObjects.Contains(collision.gameObject))
+        {
+            return;
+        }
+
+        hitObjects.Add(collision.gameObject);
+        speed *= -1;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        hitObjects.Remove(collision.gameObject);
     }
 }
